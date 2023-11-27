0c799e0fd1100c6eec4521edd5842b0aceea9e54
solomonbaez <solomon.james.baez@gmail.com>
2023-11-26 23:59:17 -0800

    Added more functionality to BookStore

--- app/BookStore.hs
+++ app/BookStore.hs
@@ -1,6 +1,12 @@
 -- BookInfo type constructor = value constructor + components
 -- Parameterised type! The compiler has no way to determine actual typing
--- BookInfo is an example of a polymorphic parameterised type
+-- Book is an example of a polymorphic parameterised type
 data BookInfo = Book Int String [String] deriving(Show)
+
+type CustomerID = Int
+type ReviewBody = String
+data BookReview = BookReview BookInfo CustomerID ReviewBody
+
+type BookRecord = (BookInfo, BookReview)
 
 programmingBook = Book 10 "Hello, Haskell" ["The guys"]
--- app/lastButOne.hs
+++ app/lastButOne.hs
@@ -1,3 +1,4 @@
+lastButOne::[a]->a
 lastButOne xs = if not (null xs) && length xs >= 2
                 then xs !! (length xs - 2)
                 else error "List is not greater than 2"
