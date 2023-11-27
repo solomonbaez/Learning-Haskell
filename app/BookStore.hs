6569cbedc6bdc0cf73c5b05614a858cf941b3bfd
solomonbaez <solomon.james.baez@gmail.com>
2023-11-26 22:04:19 -0800

    Added learning notes

--- app/BookStore.hs
+++ app/BookStore.hs
@@ -1,4 +1,6 @@
 -- BookInfo type constructor = value constructor + components
+-- Parameterised type! The compiler has no way to determine actual typing
+-- BookInfo is an example of a polymorphic parameterised type
 data BookInfo = Book Int String [String] deriving(Show)
 
 programmingBook = Book 10 "Hello, Haskell" ["The guys"]
--- app/myDrop.hs
+++ app/myDrop.hs
@@ -1,3 +1,9 @@
+--function declaration matches filename 
+--interesting to consider that short circuiting the if/then/else
+--means it's reasonable to have early failures occur on the left
+--hand side of the expression
+--
+--Additionally this expression evaluates RECURSIVELY
 myDrop n xs = if n <= 0 || null xs
               then xs
-              else myDrop (n - 1) (tail xs)
+              else myDrop (n - 1) (tail xs) 
