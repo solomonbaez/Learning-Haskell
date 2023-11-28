21260d2bd08eb071812b2c7e4d07ab3ebb56409b
solomonbaez <solomon.james.baez@gmail.com>
2023-11-28 01:15:35 -0800

    Experimenting with type records and matching

--- app/BookStore.hs
+++ app/BookStore.hs
@@ -1,7 +1,11 @@
 -- BookInfo type constructor = value constructor + components
 -- Parameterised type! The compiler has no way to determine actual typing
 -- Book is an example of a polymorphic parameterised type
-data BookInfo = Book Int String [String] deriving(Show)
+data BookInfo = Book{
+    bookID::Int,
+    bookTitle::String,
+    bookAuthors::[String]
+  } deriving(Show)
 
 type CustomerID = Int
 type ReviewBody = String
@@ -10,3 +14,6 @@ data BookReview = BookReview BookInfo CustomerID ReviewBody
 type BookRecord = (BookInfo, BookReview)
 
 programmingBook = Book 10 "Hello, Haskell" ["The guys"]
+
+getTitle (book) = bookTitle book
+
