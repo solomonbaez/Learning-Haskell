10ef901da26c532c1326d5fc390011e0a2f74f75
solomonbaez <solomon.james.baez@gmail.com>
2023-11-28 23:54:15 -0800

    Expanded linked list definitions and comparisons

--- app/RecursiveTypes.hs
+++ app/RecursiveTypes.hs
@@ -1,10 +1,16 @@
 --the type List is defined by itself
 --leading to potentially infinite recursion
+--
+--a simple singly linked list implementation
 data List a = Cons a (List a) | Nil deriving(Show)
 
 --demonstration that List is acceptable as a list type
 fromList (x:xs) = Cons x (fromList xs)
 fromList []     = Nil
+
+--reverse demonstration that the list type is acceptable as a List 
+toList (Cons x xs) = x : toList xs
+toList Nil         = []
 
 --simple binar tree implementation
 data BinaryTree a = Node a (BinaryTree a) (BinaryTree a)
