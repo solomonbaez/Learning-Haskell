5c2347368c43bea12e770121d4748357118583c5
solomonbaez <solomon.james.baez@gmail.com>
2023-11-29 17:32:53 -0800

    Working through list operations -> incl ranges and comprehension

--- basics/BasicFunctions.hs
+++ basics/BasicFunctions.hs
@@ -11,3 +11,24 @@ doubleMe x = x + x
 doubleSmallNumber x = if x > 100
                         then x
                         else doubleMe x
+
+--strings are lists in haskell
+--list concatenation is exhaustive
+--i.e. 
+--to add two lists we work through every element
+--
+--using the cons : operator we can quickly append
+smallConcat x xs = x:xs
+
+getHead xs = if length xs > 0
+              then head xs
+              else Nothing
+
+--ranges are useful but can be tricky
+--infinite lists can be created with [a, b..]
+infiniteSlice pos = take pos [0, 1..]
+
+--list comprehension in action
+getEvenNums maxNum = [x*2 | x <- [1..maxNum]]
+
+oddBoomBangs xs = [if x < 10 then "BOOM!" else "BANG!" | x <-xs, odd x]
