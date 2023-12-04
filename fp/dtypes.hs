c6cc856a785a274d3acf86b1abf32c63ee3a42f5
solomonbaez <solomon.james.baez@gmail.com>
2023-12-04 15:27:54 -0800

    Deeper nesting of custom data types

--- fp/dtypes.hs
+++ fp/dtypes.hs
@@ -3,9 +3,17 @@
 data Bool = False | True
 
 --value constructors (such as Circle) are functions that produce a type
-data Shape = Circle Float Float Float | Rectangle Float Float Float Float
+data Point = Point Float Float 
+  deriving (Show)
+data Shape = Circle Point Float | Rectangle Point Point 
+  deriving (Show)
 
 surface :: Shape -> Float
-surface (Circle _ _ r) = pi * r ^ 2
-surface (Rectangle x1 y1 x2 y2) = (abs $ x2 - x1) * (abs $ y2 - y1)
+surface (Circle _ r) = pi * r ^ 2
+surface (Rectangle (Point x1 y1) (Point x2 y2)) = (abs $ x2 - x1) * (abs $ y2 - y1)
 
+transform :: Shape -> Float -> Float -> Shape
+transform (Circle (Point x y) r) tx ty = 
+  Circle (Point (x + tx) (y + ty)) r
+transform (Rectangle (Point x1 y1) (Point x2 y2)) tx ty = 
+  Rectangle (Point (x1 + tx) (y1 + ty)) (Point (x2 + tx) (y2 + ty))
